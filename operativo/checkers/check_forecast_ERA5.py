49fad9a4794b2e2e641645beafdb3d614514966e
Daniel Bonhaure <danielbonhaure@gmail.com>
2025-08-25 15:15:12 +0000

    refactor: restructuración de carpetas

---
+++ operativo/checkers/check_forecast_ERA5.py
@@ -0,0 +1,88 @@
+
+import os
+import sys
+
+import xarray as xr
+import pandas as pd
+import datetime as dt
+
+from pathlib import Path
+
+try:
+    from setup.config import  GlobalConfig
+    from funciones_extra import mapa_chequeo
+except ImportError:
+    sys.path.append(
+        os.fspath(Path(__file__).parent.parent)
+    )
+    from setup.config import  GlobalConfig
+    from funciones_extra import mapa_chequeo
+
+
+if __name__ == '__main__':
+
+    # Leer archivo de configuración
+    config = GlobalConfig.Instance().app_config
+    # Definir carpetas con datos y figuras
+    carpeta_datos = os.fspath(Path(config.carpeta_datos))
+    carpeta_figuras = os.fspath(Path(config.carpeta_figuras))
+
+    #
+    # INICIAR CHECKEO
+    #
+
+    d0 = xr.open_dataset(f'{carpeta_datos}/operativo/forecast/tas/202501220000/tas_GEFSv12_CPC_202501220000_forecast.nc').tas
+    print(d0)
+
+    fi = [dt.datetime(2025,1,23), dt.datetime(2025,1,30), dt.datetime(2025,2,6)]
+    ff = [dt.datetime(2025,1,29), dt.datetime(2025,2,5), dt.datetime(2025,2,19)]
+    ds = xr.open_dataset(f'{carpeta_datos}/2025_diario.nc')
+    t2m = ds.t2m
+    t_week = t2m.sortby('valid_time', ascending=False).rolling(valid_time=7, min_periods=1).mean().sortby('valid_time', ascending=True)
+    t_2week = t2m.sortby('valid_time', ascending=False).rolling(valid_time=14, min_periods=1).mean().sortby('valid_time', ascending=True)
+    ds0 = xr.open_dataset(f'{carpeta_datos}/clim/tmean/tmean_weeklymean_pctile80_smooth.nc').tmean
+    ds1 = xr.open_dataset(f'{carpeta_datos}/clim/tmean/tmean_2weeklymean_pctile80_smooth.nc').tmean
+
+
+    c_out = f'{carpeta_figuras}/operativo/tas/202501220000/'
+
+    for fecha, fecha_f, semana in zip(fi, ff, ['semana1', 'semana2', 'semana3y4']):
+        ####
+        start_date = pd.to_datetime(d0['S'].values[0])  # Get the starting date
+        target_date_pd = pd.to_datetime(fecha) + dt.timedelta(hours=12)
+        dates = [start_date + td for td in d0['L'].values]
+        # 2. Find the index of the target date
+        target_index = dates.index(target_date_pd)
+        da_selected = d0.isel({'L': target_index})
+        print(da_selected)
+        ###
+
+        f_old = fecha.replace(year=1960)
+        if semana == 'semana3y4':
+            dato = t_2week.sel(valid_time=fecha)
+            dato = dato.rename({'longitude': 'X','latitude': 'Y'})
+            dato = dato.interp_like(da_selected)
+            print(dato)
+            percentil = ds1.sel(S=f_old)
+            new_index = [fecha]
+            percentil = percentil.assign_coords(S=fecha)
+            percentil = percentil.rename({'longitude': 'X','latitude': 'Y'})
+            percentil = percentil.interp_like(da_selected)
+        else:
+            dato = t_week.sel(valid_time=fecha)
+            dato = dato.rename({'longitude': 'X','latitude': 'Y'})
+            dato = dato.interp_like(da_selected)
+            print(dato)
+            percentil = ds0.sel(S=f_old)
+            new_index = [fecha]
+            percentil = percentil.assign_coords(S=fecha)
+            percentil = percentil.rename({'longitude': 'X','latitude': 'Y'})
+            percentil = percentil.interp_like(da_selected)
+        #
+        print(percentil)
+
+        #
+        comparison = dato > percentil
+
+        nome_fig = c_out + 'chequeo_pronostico_corregido_semana_' + semana + '.jpg'
+        mapa_chequeo(comparison, fecha, fecha_f, nome_fig)
---
+++ operativo/extras/make_colorbar.py
@@ -0,0 +1,73 @@
+
+import os
+import sys
+
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import matplotlib.colorbar
+import numpy as np
+
+from pathlib import Path
+from matplotlib import colors as c
+
+try:
+    from setup.config import  GlobalConfig
+    from funciones_extra import mapa_chequeo
+except ImportError:
+    sys.path.append(
+        os.fspath(Path(__file__).parent.parent)
+    )
+    from setup.config import  GlobalConfig
+    from funciones_extra import mapa_chequeo
+
+
+if __name__ == '__main__':
+
+    # Leer archivo de configuración
+    config = GlobalConfig.Instance().app_config
+    # Definir carpetas con figuras
+    carpeta_figuras = os.fspath(Path(config.carpeta_figuras).parent)
+
+    #
+    # INICIAR GENERACIÓN DE COLORBAR
+    #
+
+    c_pp = ['#ffffff', '#ffeda0','#fed976','#feb24c','#fd8d3c','#fc4e2a','#e31a1c','#bd0026','#800026']
+    c_map = c.ListedColormap(c_pp)
+    bounds = np.array([0, 20.01, 30.01, 40.01, 50.01, 60.01, 70.01, 80.01, 90.01, 100.01])
+    norm = c.BoundaryNorm(boundaries=bounds, ncolors=len(c_pp))
+    tick_l = ['0', '20', '30', '40', '50', '60', '70', '80', '90', '100']
+
+
+    ###################
+    fig = plt.figure()
+    ax = fig.add_axes((0.05, 0.80, 0.1, 0.9))
+
+    cb = mpl.colorbar.ColorbarBase(ax, orientation='vertical',
+                                   cmap=c_map,
+                                   norm=norm,
+                                   ticks=bounds,
+                                   drawedges=True)
+
+    cb.ax.yaxis.set_ticks_position('left')
+    cb.ax.set_title('Prob. (%)', weight='bold')
+    cb.ax.set_yticklabels(tick_l, weight='bold', fontsize=9)
+    plt.savefig(f'{carpeta_figuras}/colorbar2_vertical_new.jpg', bbox_inches='tight')
+    plt.close(fig)
+
+
+    ###################
+    fig = plt.figure()
+    ax = fig.add_axes((0.05, 0.80, 0.9, 0.1))
+
+    cb = mpl.colorbar.ColorbarBase(ax, orientation='horizontal',
+                                   cmap=c_map,
+                                   norm=norm,
+                                   ticks=bounds,
+                                   drawedges=True)
+
+    cb.ax.xaxis.set_ticks_position('bottom')
+    cb.ax.set_title('Prob. (%)', weight='bold')
+    cb.ax.set_xticklabels(tick_l, weight='bold', fontsize=9)
+    plt.savefig(f'{carpeta_figuras}/colorbar2_horizontal_new.jpg', bbox_inches='tight')
+    plt.close(fig)
